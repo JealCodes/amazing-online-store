30972616162c25e3c172d37ebb22dda183797054
JealDev <jeal.devs@gmail.com>
2026-05-09 13:17:05 -0500

    Fix bug blocks carousel and grid

--- includes/core/register-blocks.php
+++ includes/core/register-blocks.php
@@ -4,17 +4,15 @@ declare(strict_types=1);
 
 function aos_register_blocks(): void
 {
-
     register_block_type(
         AOS_PLUGIN_PATH .
-        'src/blocks/product-grid'
+        'build/blocks/product-grid'
     );
 
     register_block_type(
         AOS_PLUGIN_PATH .
-        'src/blocks/product-carousel'
+        'build/blocks/product-carousel'
     );
-
 }
 
 add_action(
